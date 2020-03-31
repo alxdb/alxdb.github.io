e413aea76b2c380d676c1b0315d08241ebe267b8
Alexander Davidson Bryan <alxdb@protonmail.com>
2020-03-31 11:41:31 +0100

    general niceness

--- hello_world.js
+++ hello_world.js
@@ -1,35 +1,35 @@
-let hello_world_container = document.getElementsByClassName("hello-world")[0];
-let hello_world_text = document.getElementsByClassName("hello-world__text")[0];
+let hello_world_el = document.getElementById("hello-world");
 
-greetings = [
-  ["c", 'printf("hello %s!\\n", visitor);'],
-  ["bash", 'echo "salut ${VISITOR}!"'],
-  ["java", 'System.out.println("hallo " + visitor.toString() + "!");'],
-  ["clojure", '(println (str "¡hola " visitor "!"))'],
-  ["rust", 'println!("你好 {}!", visitor);'],
-  ["python", 'print("звать {}!".format(visitor))'],
-  ["haskell", 'putStrLn $ "Terve " ++ visitor ++ "!"']
+let greetings = [
+    ["c", 'printf("hello %s!\\n", visitor);'],
+    ["bash", 'echo "salut ${VISITOR}!"'],
+    ["java", 'System.out.println("hallo " + visitor.toString() + "!");'],
+    ["clojure", '(println (str "¡hola " visitor "!"))'],
+    ["rust", 'println!("你好 {}!", visitor);'],
+    ["python", 'print("звать {}!".format(visitor))'],
+    ["haskell", 'putStrLn $ "terve " ++ visitor ++ "!"']
 ];
 
 function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 let prev_greeting;
+
 function setGreeting() {
-  let index = getRandomInt(0, greetings.length - 1);
-  // Don't show a greeting twice in a row
-  while (prev_greeting == index) {
-    index = getRandomInt(0, greetings.length - 1);
-  }
-  prev_greeting = index;
-  let greeting = greetings[index];
-  hello_world_text.innerHTML = "<pre>" + greeting[1] + "</pre>";
+    let index = getRandomInt(0, greetings.length - 1);
+    // Don't show a greeting twice in a row
+    while (prev_greeting == index) {
+        index = getRandomInt(0, greetings.length - 1);
+    }
+    prev_greeting = index;
+    let greeting = greetings[index];
+    hello_world_el.textContent = greeting[1];
 }
 
 setGreeting();
-setInterval(() => {
-  setGreeting();
-}, 5000);
+window.setInterval(() => {
+    setGreeting();
+}, 2500);
